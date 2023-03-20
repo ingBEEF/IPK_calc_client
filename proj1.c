5a4a11a2d30531136e77a5d46258e09f9ae94af1
ingBEEF <xvelic05@stud.fit.vutbr.cz>
2023-03-20 16:25:19 +0100

    feat: basic udp baf

--- proj1.c
+++ proj1.c
@@ -10,19 +10,48 @@
 #include <unistd.h>
 
 #define BUFSIZE 512
+
+void udp_mess (struct sockaddr_in server_address) {
+    char buf[BUFSIZE];
+    /* Vytvoreni soketu */ 
+	if ((client_socket = socket(AF_INET, SOCK_DGRAM, 0)) <= 0)
+	{
+		perror("ERROR: socket");
+		exit(EXIT_FAILURE);
+	}
+    while (1){
+        /* nacteni zpravy od uzivatele */
+        bzero(buf, BUFSIZE);
+        printf("Please enter msg: ");
+        fgets(buf + 2, BUFSIZE - 2, stdin);
+        if (strcmp((buf), EOF))
+        buf[0] = 0x00;
+        buf[1] = strlen(buf + 2)
+
+        /* odeslani zpravy na server */
+        serverlen = sizeof(server_address);
+        bytestx = sendto(client_socket, buf, strlen(buf + 2) + 2, 0, (struct sockaddr *) &server_address, serverlen);
+        if (bytestx < 0) 
+        perror("ERROR: sendto");
+        
+        /* prijeti odpovedi a jeji vypsani */
+        bytesrx = recvfrom(client_socket, buf, BUFSIZE, 0, (struct sockaddr *) &server_address, &serverlen);
+        if (bytesrx < 0) 
+        perror("ERROR: recvfrom");
+        buf[5] = 0;
+        printf("Echo from server: %s", buf+3);
+    }
+    return 0;
+}
+
 int main (int argc, const char * argv[]) {
 	int client_socket, port_number, bytestx, bytesrx;
     socklen_t serverlen;
     const char *server_hostname;
     struct hostent *server;
     struct sockaddr_in server_address;
-    char buf[BUFSIZE];
      
     /* 1. test vstupnich parametru: */
-    if (argc != 3) {
-       fprintf(stderr,"usage: %s <hostname> <port>\n", argv[0]);
-       exit(EXIT_FAILURE);
-    }
     server_hostname = argv[2];
     port_number = atoi(argv[4]);
     
@@ -42,28 +71,12 @@ int main (int argc, const char * argv[]) {
     /* tiskne informace o vzdalenem soketu */ 
     printf("INFO: Server socket: %s : %d \n", inet_ntoa(server_address.sin_addr), ntohs(server_address.sin_port));
     
-    /* Vytvoreni soketu */ 
-	if ((client_socket = socket(AF_INET, SOCK_DGRAM, 0)) <= 0)
-	{
-		perror("ERROR: socket");
-		exit(EXIT_FAILURE);
-	}
-	    
-    /* nacteni zpravy od uzivatele */
-    bzero(buf, BUFSIZE);
-    printf("Please enter msg: ");
-    fgets(buf, BUFSIZE, stdin);
-
-    /* odeslani zpravy na server */
-    serverlen = sizeof(server_address);
-    bytestx = sendto(client_socket, buf, strlen(buf), 0, (struct sockaddr *) &server_address, serverlen);
-    if (bytestx < 0) 
-      perror("ERROR: sendto");
+	
+    if (!(strcmp(argv[6], "tcp"))){
+        //tcp_mess();
+    }
+    else if (!(strcmp(argv[6], "udp"))){
+        udp_mess(server_address);
+    }
     
-    /* prijeti odpovedi a jeji vypsani */
-    bytesrx = recvfrom(client_socket, buf, BUFSIZE, 0, (struct sockaddr *) &server_address, &serverlen);
-    if (bytesrx < 0) 
-      perror("ERROR: recvfrom");
-    printf("Echo from server: %s", buf);
-    return 0;
 }
